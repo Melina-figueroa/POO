58dbad7fbd0a5979f157907e82938dc07b54d4d0
melina <melinafigueroa@tdf.edu.ar>
2022-09-11 19:07:31 -0300

    creacion de carpetas

---
+++ primerClasePOO/ejercicios de clase/Leandro/persona.ts
@@ -0,0 +1,28 @@
+//Atributos o estados//Siempre van arriba//
+class Persona {
+    nombre: string;
+    apellido: string;
+    dni: number;
+    edad: number;
+    vivo: boolean;
+
+    //Funcionalidades//
+
+    hablar(){
+
+        console.log("Hola");
+    }
+        razonar(){
+            let numero1: number = 5;
+            let numero2: number = 5;
+            console.log(numero1 + numero2);
+       
+        
+    }
+
+}
+
+
+
+
+
